0b0cedf0928f7e70dac31fd10c14b84b0417336e
Jan Manikowski <janek210102j@gmail.com>
2025-06-16 01:01:23 +0200

    start

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Dropship-Mobile/', // ← your repo name here
+  // base: '/Dropship-Mobile/',
   plugins: [react()],
 });
